16fda9f77ea9cfe094c4ab6c4e05902991211210
Johannes Stensfelt <johannes.stensfelt@avanade.com>
2019-08-30 11:10:39 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import EreFrancis from './EreFrancis';
+import NoMatch from './NoMatch';
 import './App.css';
 
 function App() {
 
-  const [date, setDate] = useState(new Date());
-
-  const isFriday = () => {
-    return date.getDay() === 5 ? true : false;
-  }
-  const hours = 15 - date.getHours();
-  const minutes = 59 - date.getMinutes();
-  const seconds = 59 - date.getSeconds();
-
-  const ereFrancis = () => {
-    if (isFriday() && hours >= 16) {
-      return "Yes";
-    }
-    else if (isFriday()) {
-      return "Yes, in " + hours + " h " + minutes + " min " + seconds + " sec";
-    }
-    else {
-      return "No";
-    }
-  }
-
-  useEffect(() => {
-    setInterval(() => setDate(new Date()), 1000)
-  });
-
   return (
-    <div className="App">
-      <h1 className="question">Time for Friends' Table?</h1>
-      <div>
-        <h2 className={isFriday() ? "green" : "red"}>
-          {ereFrancis()}
-        </h2>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={EreFrancis} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/EreFrancis.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function EreFrancis() {
+
+  const [date, setDate] = useState(new Date());
+
+  const isFriday = () => {
+    return date.getDay() === 5 ? true : false;
+  }
+  const hours = 15 - date.getHours();
+  const minutes = 59 - date.getMinutes();
+  const seconds = 59 - date.getSeconds();
+
+  const ereFrancis = () => {
+    if (isFriday() && hours >= 16) {
+      return "Yes";
+    }
+    else if (isFriday()) {
+      return "Yes, in " + hours + " h " + minutes + " min " + seconds + " sec";
+    }
+    else {
+      return "No";
+    }
+  }
+
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000)
+  });
+
+  return (
+    <div>
+      <h1 className="question">Time for Friends' Table?</h1>
+      <div>
+        <h2 className={isFriday() ? "green" : "red"}>
+          {ereFrancis()}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default EreFrancis;
